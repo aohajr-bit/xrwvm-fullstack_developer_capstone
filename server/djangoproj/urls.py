8a10dd9da675deebf3896708dc64c00025e8d80b
aohajr-bit <aohajr@gmail.com>
2026-03-03 10:34:45 -0600

    Refactor URL patterns and fix formatting issues

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 
     # Frontend page routes (/login, /dealers, /dealer/<id>, /postreview/<id>)
-    path('', include('djangoapp.urls')),
+    path("", include("djangoapp.urls")),
 
     # API routes (/djangoapp/login, /djangoapp/get_cars, /djangoapp/add_review, etc.)
-    path('djangoapp/', include('djangoapp.api_urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("djangoapp/", include("djangoapp.api_urls")),
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT,
+)
